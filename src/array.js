645a84c8f59c0b53f2e41ae9befe6a6f4ce105d5
kbdaine <kbpunzalan@up.edu.ph>
2022-05-05 15:39:59 +0800

    final

--- src/array.js
+++ src/array.js
@@ -3,6 +3,7 @@ import ponzy from './pics/friends/ponzy.jpg'
 import buknoy from './pics/friends/buknoy.jpg'
 import merlita from './pics/friends/merlita.jpg'
 import florencia from './pics/friends/florencia.jpg'
+import jake from './pics/friends/jake.jpg'
 import yor from './pics/friends/yor.jpg'
 
 import earth from './pics/ads/earth.jpg'
@@ -14,13 +15,15 @@ const myFriends = [
     { name: "Buknoy di Mauga", image: buknoy, mutual: 69, id: 3 },
     { name: "Merlita Ligalig", image: merlita, mutual: 666, id: 4}, 
     { name: "Florencia Balimbing", image: florencia, mutual: 75, id: 5},
-    { name: "Yor Cutie", image: yor, mutual: 9999999, id: 6} 
+    { name: "Jake de Dog", image: jake, mutual: 101, id: 6},
+    { name: "Yor Cutie", image: yor, mutual: 9999999, id: 7}
+
 ];
 
 const friendPost = [
     { author: myFriends[3].name, image: myFriends[3].image, timestamp: "01/18/2022 | 11:37", content: "Nak ung cnaing! Pataye n at bka msunog!" },
     { author: myFriends[3].name, image: myFriends[3].image, timestamp: "01/18/2022 | 11:40",content: "Sorry, Metuhh friends. I thought the previous post was a direct message. Will delete the post later." },
-    { author: myFriends[5].name, image: myFriends[5].image, timestamp: "05/05/2022 | 16:19", content: "Anime Waifus for LENI-KIKO! 🌸🌸" }
+    { author: myFriends[6].name, image: myFriends[6].image, timestamp: "05/05/2022 | 16:19", content: "Anime Waifus for LENI-KIKO! 🌸🌸" }
 ];
 
 
